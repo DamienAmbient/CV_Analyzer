760b140096528e3191c6ffba4c681e2b60d74e3f
Damien <testingmern7@gmail.com>
2025-09-11 14:31:16 +0100

    navbar + resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume AI Check" },
+    { name: "description", content: "Check your CV with AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar></Navbar>
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
