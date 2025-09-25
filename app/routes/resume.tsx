ffebf238783dcc8b889bee96d0a2f6bb0fbf52a6
Damien <damienambient@gmail.com>
2025-09-25 13:49:07 +0100

    resume feedback page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
@@ -12,8 +15,13 @@ const Resume = () => {
   const { id } = useParams();
   const [imageUrl, setImageUrl] = useState("");
   const [resumeUrl, setResumeUrl] = useState("");
-  const [feedback, setFeedback] = useState("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !auth.isAuthenticated)
+      navigate(`/auth?next=/resume/${id}`);
+  }, [isLoading]);
 
   useEffect(() => {
     const loadResume = async () => {
@@ -63,6 +71,25 @@ const Resume = () => {
                 />
               </a>
             </div>
+          )}
+        </section>
+        <section className="feedback-section">
+          <h2 className="text-4xl text-black font-bold">Resume Review</h2>
+          {feedback ? (
+            <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+              <Summary feedback={feedback}></Summary>
+              <ATS
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              ></ATS>
+              <Details feedback={feedback}></Details>
+            </div>
+          ) : (
+            <img
+              src="/images/resume-scan2.gif"
+              className="w-full"
+              alt="resume-scanning"
+            ></img>
           )}
         </section>
       </div>
